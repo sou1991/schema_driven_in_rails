a5e03834b5d493efc8f665ad1e37d58a483416f6
sou1991 <55009005+sou1991@users.noreply.github.com>
2023-12-05 09:11:21 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::API
   before_action :authenticated?
   
   def authenticated?
-    render status: 401, json:{error: 'Unauthorized'} if request.headers[:Authorization] != 'Barer abcdefg123456'
+    render status: 401, json:{error: 'Unauthorized'} if request.headers[:Authorization] != 'Bearer abcdefg123456'
   end
 end
